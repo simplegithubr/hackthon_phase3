6ab6a71fd452990ea3e3cf6cb7d9b7c31df153be
sagar sheikh <160107305+simplegithubr@users.noreply.github.com>
2026-01-19 08:49:31 +0500

    Update next.config.js to include NEXT_PUBLIC_API_URL for Vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // swcMinify: true,
+// }
+// module.exports = nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 }
+
 module.exports = nextConfig
